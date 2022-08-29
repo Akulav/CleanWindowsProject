f9a7a3d9e8912d3eabe3b2d666b8e8b048719b6d
Akulav <43496076+Akulav@users.noreply.github.com>
2022-08-29 07:49:57 +0300

    Initial Release

---
+++ CleanWindowsProject/MainForm.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace CleanWindowsProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edgeRemoveBtn = new System.Windows.Forms.Button();
+            this.discordBtn = new System.Windows.Forms.Button();
+            this.installPanel = new System.Windows.Forms.Panel();
+            this.telegramBtn = new System.Windows.Forms.Button();
+            this.steamBtn = new System.Windows.Forms.Button();
+            this.bcuninstallerBtn = new System.Windows.Forms.Button();
+            this.visualstudioBtn = new System.Windows.Forms.Button();
+            this.installAllBtn = new System.Windows.Forms.Button();
+            this.wemodBtn = new System.Windows.Forms.Button();
+            this.windirstatBtn = new System.Windows.Forms.Button();
+            this.vlcBtn = new System.Windows.Forms.Button();
+            this.veracryptBtn = new System.Windows.Forms.Button();
+            this.gpuzBtn = new System.Windows.Forms.Button();
+            this.qbitBtn = new System.Windows.Forms.Button();
+            this.notepadBtn = new System.Windows.Forms.Button();
+            this.firefoxBtn = new System.Windows.Forms.Button();
+            this.keepassBtn = new System.Windows.Forms.Button();
+            this.libreBtn = new System.Windows.Forms.Button();
+            this.githubBtn = new System.Windows.Forms.Button();
+            this.irfanBtn = new System.Windows.Forms.Button();
+            this.cpuzBtn = new System.Windows.Forms.Button();
+            this.iTunesBtn = new System.Windows.Forms.Button();
+            this.zipBtn = new System.Windows.Forms.Button();
+            this.Installs = new System.Windows.Forms.Label();
+            this.tweakPanel = new System.Windows.Forms.Panel();
+            this.tweakLabel = new System.Windows.Forms.Label();
+            this.installPanel.SuspendLayout();
+            this.tweakPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // edgeRemoveBtn
+            // 
+            this.edgeRemoveBtn.Location = new System.Drawing.Point(73, 19);
+            this.edgeRemoveBtn.Name = "edgeRemoveBtn";
+            this.edgeRemoveBtn.Size = new System.Drawing.Size(138, 23);
+            this.edgeRemoveBtn.TabIndex = 0;
+            this.edgeRemoveBtn.Text = "Remove Edge";
+            this.edgeRemoveBtn.UseVisualStyleBackColor = true;
+            this.edgeRemoveBtn.Click += new System.EventHandler(this.edgeRemoveBtn_Click);
+            // 
+            // discordBtn
+            // 
+            this.discordBtn.Location = new System.Drawing.Point(104, 19);
+            this.discordBtn.Name = "discordBtn";
+            this.discordBtn.Size = new System.Drawing.Size(138, 23);
+            this.discordBtn.TabIndex = 3;
+            this.discordBtn.Text = "Discord";
+            this.discordBtn.UseVisualStyleBackColor = true;
+            this.discordBtn.Click += new System.EventHandler(this.discordBtn_Click);
+            // 
+            // installPanel
+            // 
+            this.installPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.installPanel.Controls.Add(this.telegramBtn);
+            this.installPanel.Controls.Add(this.steamBtn);
+            this.installPanel.Controls.Add(this.bcuninstallerBtn);
+            this.installPanel.Controls.Add(this.visualstudioBtn);
+            this.installPanel.Controls.Add(this.installAllBtn);
+            this.installPanel.Controls.Add(this.wemodBtn);
+            this.installPanel.Controls.Add(this.windirstatBtn);
+            this.installPanel.Controls.Add(this.vlcBtn);
+            this.installPanel.Controls.Add(this.veracryptBtn);
+            this.installPanel.Controls.Add(this.gpuzBtn);
+            this.installPanel.Controls.Add(this.qbitBtn);
+            this.installPanel.Controls.Add(this.notepadBtn);
+            this.installPanel.Controls.Add(this.firefoxBtn);
+            this.installPanel.Controls.Add(this.keepassBtn);
+            this.installPanel.Controls.Add(this.libreBtn);
+            this.installPanel.Controls.Add(this.githubBtn);
+            this.installPanel.Controls.Add(this.irfanBtn);
+            this.installPanel.Controls.Add(this.cpuzBtn);
+            this.installPanel.Controls.Add(this.iTunesBtn);
+            this.installPanel.Controls.Add(this.zipBtn);
+            this.installPanel.Controls.Add(this.Installs);
+            this.installPanel.Controls.Add(this.discordBtn);
+            this.installPanel.Location = new System.Drawing.Point(12, 12);
+            this.installPanel.Name = "installPanel";
+            this.installPanel.Size = new System.Drawing.Size(492, 356);
+            this.installPanel.TabIndex = 7;
+            // 
+            // telegramBtn
+            // 
+            this.telegramBtn.Location = new System.Drawing.Point(248, 280);
+            this.telegramBtn.Name = "telegramBtn";
+            this.telegramBtn.Size = new System.Drawing.Size(138, 23);
+            this.telegramBtn.TabIndex = 24;
+            this.telegramBtn.Text = "Telegram";
+            this.telegramBtn.UseVisualStyleBackColor = true;
+            this.telegramBtn.Click += new System.EventHandler(this.telegramBtn_Click);
+            // 
+            // steamBtn
+            // 
+            this.steamBtn.Location = new System.Drawing.Point(104, 280);
+            this.steamBtn.Name = "steamBtn";
+            this.steamBtn.Size = new System.Drawing.Size(138, 23);
+            this.steamBtn.TabIndex = 23;
+            this.steamBtn.Text = "Steam";
+            this.steamBtn.UseVisualStyleBackColor = true;
+            this.steamBtn.Click += new System.EventHandler(this.steamBtn_Click);
+            // 
+            // bcuninstallerBtn
+            // 
+            this.bcuninstallerBtn.Location = new System.Drawing.Point(248, 251);
+            this.bcuninstallerBtn.Name = "bcuninstallerBtn";
+            this.bcuninstallerBtn.Size = new System.Drawing.Size(138, 23);
+            this.bcuninstallerBtn.TabIndex = 22;
+            this.bcuninstallerBtn.Text = "BCUninstaller";
+            this.bcuninstallerBtn.UseVisualStyleBackColor = true;
+            this.bcuninstallerBtn.Click += new System.EventHandler(this.bcuninstallerBtn_Click);
+            // 
+            // visualstudioBtn
+            // 
+            this.visualstudioBtn.Location = new System.Drawing.Point(104, 251);
+            this.visualstudioBtn.Name = "visualstudioBtn";
+            this.visualstudioBtn.Size = new System.Drawing.Size(138, 23);
+            this.visualstudioBtn.TabIndex = 21;
+            this.visualstudioBtn.Text = "Visual Studio";
+            this.visualstudioBtn.UseVisualStyleBackColor = true;
+            this.visualstudioBtn.Click += new System.EventHandler(this.visualstudioBtn_Click);
+            // 
+            // installAllBtn
+            // 
+            this.installAllBtn.Location = new System.Drawing.Point(104, 309);
+            this.installAllBtn.Name = "installAllBtn";
+            this.installAllBtn.Size = new System.Drawing.Size(138, 23);
+            this.installAllBtn.TabIndex = 20;
+            this.installAllBtn.Text = "Install All";
+            this.installAllBtn.UseVisualStyleBackColor = true;
+            this.installAllBtn.Click += new System.EventHandler(this.installAllBtn_Click);
+            // 
+            // wemodBtn
+            // 
+            this.wemodBtn.Location = new System.Drawing.Point(248, 222);
+            this.wemodBtn.Name = "wemodBtn";
+            this.wemodBtn.Size = new System.Drawing.Size(138, 23);
+            this.wemodBtn.TabIndex = 19;
+            this.wemodBtn.Text = "Wemod";
+            this.wemodBtn.UseVisualStyleBackColor = true;
+            this.wemodBtn.Click += new System.EventHandler(this.wemodBtn_Click);
+            // 
+            // windirstatBtn
+            // 
+            this.windirstatBtn.Location = new System.Drawing.Point(248, 193);
+            this.windirstatBtn.Name = "windirstatBtn";
+            this.windirstatBtn.Size = new System.Drawing.Size(138, 23);
+            this.windirstatBtn.TabIndex = 18;
+            this.windirstatBtn.Text = "Windirstat";
+            this.windirstatBtn.UseVisualStyleBackColor = true;
+            this.windirstatBtn.Click += new System.EventHandler(this.windirstatBtn_Click);
+            // 
+            // vlcBtn
+            // 
+            this.vlcBtn.Location = new System.Drawing.Point(248, 164);
+            this.vlcBtn.Name = "vlcBtn";
+            this.vlcBtn.Size = new System.Drawing.Size(138, 23);
+            this.vlcBtn.TabIndex = 17;
+            this.vlcBtn.Text = "VLC";
+            this.vlcBtn.UseVisualStyleBackColor = true;
+            this.vlcBtn.Click += new System.EventHandler(this.vlcBtn_Click);
+            // 
+            // veracryptBtn
+            // 
+            this.veracryptBtn.Location = new System.Drawing.Point(248, 135);
+            this.veracryptBtn.Name = "veracryptBtn";
+            this.veracryptBtn.Size = new System.Drawing.Size(138, 23);
+            this.veracryptBtn.TabIndex = 16;
+            this.veracryptBtn.Text = "Veracrypt";
+            this.veracryptBtn.UseVisualStyleBackColor = true;
+            this.veracryptBtn.Click += new System.EventHandler(this.veracryptBtn_Click);
+            // 
+            // gpuzBtn
+            // 
+            this.gpuzBtn.Location = new System.Drawing.Point(248, 106);
+            this.gpuzBtn.Name = "gpuzBtn";
+            this.gpuzBtn.Size = new System.Drawing.Size(138, 23);
+            this.gpuzBtn.TabIndex = 15;
+            this.gpuzBtn.Text = "GPU-Z";
+            this.gpuzBtn.UseVisualStyleBackColor = true;
+            this.gpuzBtn.Click += new System.EventHandler(this.gpuzBtn_Click);
+            // 
+            // qbitBtn
+            // 
+            this.qbitBtn.Location = new System.Drawing.Point(248, 77);
+            this.qbitBtn.Name = "qbitBtn";
+            this.qbitBtn.Size = new System.Drawing.Size(138, 23);
+            this.qbitBtn.TabIndex = 14;
+            this.qbitBtn.Text = "qBittorent";
+            this.qbitBtn.UseVisualStyleBackColor = true;
+            this.qbitBtn.Click += new System.EventHandler(this.qbitBtn_Click);
+            // 
+            // notepadBtn
+            // 
+            this.notepadBtn.Location = new System.Drawing.Point(248, 48);
+            this.notepadBtn.Name = "notepadBtn";
+            this.notepadBtn.Size = new System.Drawing.Size(138, 23);
+            this.notepadBtn.TabIndex = 13;
+            this.notepadBtn.Text = "Notepad++";
+            this.notepadBtn.UseVisualStyleBackColor = true;
+            this.notepadBtn.Click += new System.EventHandler(this.notepadBtn_Click);
+            // 
+            // firefoxBtn
+            // 
+            this.firefoxBtn.Location = new System.Drawing.Point(248, 19);
+            this.firefoxBtn.Name = "firefoxBtn";
+            this.firefoxBtn.Size = new System.Drawing.Size(138, 23);
+            this.firefoxBtn.TabIndex = 12;
+            this.firefoxBtn.Text = "FireFox";
+            this.firefoxBtn.UseVisualStyleBackColor = true;
+            this.firefoxBtn.Click += new System.EventHandler(this.firefoxBtn_Click);
+            // 
+            // keepassBtn
+            // 
+            this.keepassBtn.Location = new System.Drawing.Point(104, 193);
+            this.keepassBtn.Name = "keepassBtn";
+            this.keepassBtn.Size = new System.Drawing.Size(138, 23);
+            this.keepassBtn.TabIndex = 11;
+            this.keepassBtn.Text = "KeePass";
+            this.keepassBtn.UseVisualStyleBackColor = true;
+            this.keepassBtn.Click += new System.EventHandler(this.keepassBtn_Click);
+            // 
+            // libreBtn
+            // 
+            this.libreBtn.Location = new System.Drawing.Point(104, 222);
+            this.libreBtn.Name = "libreBtn";
+            this.libreBtn.Size = new System.Drawing.Size(138, 23);
+            this.libreBtn.TabIndex = 10;
+            this.libreBtn.Text = "LibreOffice";
+            this.libreBtn.UseVisualStyleBackColor = true;
+            this.libreBtn.Click += new System.EventHandler(this.libreBtn_Click);
+            // 
+            // githubBtn
+            // 
+            this.githubBtn.Location = new System.Drawing.Point(104, 135);
+            this.githubBtn.Name = "githubBtn";
+            this.githubBtn.Size = new System.Drawing.Size(138, 23);
+            this.githubBtn.TabIndex = 9;
+            this.githubBtn.Text = "GithubDesktop";
+            this.githubBtn.UseVisualStyleBackColor = true;
+            this.githubBtn.Click += new System.EventHandler(this.githubBtn_Click);
+            // 
+            // irfanBtn
+            // 
+            this.irfanBtn.Location = new System.Drawing.Point(104, 164);
+            this.irfanBtn.Name = "irfanBtn";
+            this.irfanBtn.Size = new System.Drawing.Size(138, 23);
+            this.irfanBtn.TabIndex = 8;
+            this.irfanBtn.Text = "IrfanView";
+            this.irfanBtn.UseVisualStyleBackColor = true;
+            this.irfanBtn.Click += new System.EventHandler(this.irfanBtn_Click);
+            // 
+            // cpuzBtn
+            // 
+            this.cpuzBtn.Location = new System.Drawing.Point(104, 106);
+            this.cpuzBtn.Name = "cpuzBtn";
+            this.cpuzBtn.Size = new System.Drawing.Size(138, 23);
+            this.cpuzBtn.TabIndex = 7;
+            this.cpuzBtn.Text = "CPU-Z";
+            this.cpuzBtn.UseVisualStyleBackColor = true;
+            this.cpuzBtn.Click += new System.EventHandler(this.cpuzBtn_Click);
+            // 
+            // iTunesBtn
+            // 
+            this.iTunesBtn.Location = new System.Drawing.Point(104, 77);
+            this.iTunesBtn.Name = "iTunesBtn";
+            this.iTunesBtn.Size = new System.Drawing.Size(138, 23);
+            this.iTunesBtn.TabIndex = 6;
+            this.iTunesBtn.Text = "iTunes";
+            this.iTunesBtn.UseVisualStyleBackColor = true;
+            this.iTunesBtn.Click += new System.EventHandler(this.iTunesBtn_Click);
+            // 
+            // zipBtn
+            // 
+            this.zipBtn.Location = new System.Drawing.Point(104, 48);
+            this.zipBtn.Name = "zipBtn";
+            this.zipBtn.Size = new System.Drawing.Size(138, 23);
+            this.zipBtn.TabIndex = 5;
+            this.zipBtn.Text = "7-Zip";
+            this.zipBtn.UseVisualStyleBackColor = true;
+            this.zipBtn.Click += new System.EventHandler(this.zipBtn_Click);
+            // 
+            // Installs
+            // 
+            this.Installs.AutoSize = true;
+            this.Installs.Location = new System.Drawing.Point(225, 3);
+            this.Installs.Name = "Installs";
+            this.Installs.Size = new System.Drawing.Size(39, 13);
+            this.Installs.TabIndex = 4;
+            this.Installs.Text = "Installs";
+            // 
+            // tweakPanel
+            // 
+            this.tweakPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tweakPanel.Controls.Add(this.tweakLabel);
+            this.tweakPanel.Controls.Add(this.edgeRemoveBtn);
+            this.tweakPanel.Location = new System.Drawing.Point(510, 12);
+            this.tweakPanel.Name = "tweakPanel";
+            this.tweakPanel.Size = new System.Drawing.Size(268, 356);
+            this.tweakPanel.TabIndex = 4;
+            // 
+            // tweakLabel
+            // 
+            this.tweakLabel.AutoSize = true;
+            this.tweakLabel.Location = new System.Drawing.Point(121, 3);
+            this.tweakLabel.Name = "tweakLabel";
+            this.tweakLabel.Size = new System.Drawing.Size(45, 13);
+            this.tweakLabel.TabIndex = 5;
+            this.tweakLabel.Text = "Tweaks";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 380);
+            this.Controls.Add(this.tweakPanel);
+            this.Controls.Add(this.installPanel);
+            this.Name = "MainForm";
+            this.Text = "CleanWindows - Made by Caty V1.0";
+            this.installPanel.ResumeLayout(false);
+            this.installPanel.PerformLayout();
+            this.tweakPanel.ResumeLayout(false);
+            this.tweakPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button edgeRemoveBtn;
+        private System.Windows.Forms.Button discordBtn;
+        private System.Windows.Forms.Panel installPanel;
+        private System.Windows.Forms.Panel tweakPanel;
+        private System.Windows.Forms.Label Installs;
+        private System.Windows.Forms.Label tweakLabel;
+        private System.Windows.Forms.Button installAllBtn;
+        private System.Windows.Forms.Button wemodBtn;
+        private System.Windows.Forms.Button windirstatBtn;
+        private System.Windows.Forms.Button vlcBtn;
+        private System.Windows.Forms.Button veracryptBtn;
+        private System.Windows.Forms.Button gpuzBtn;
+        private System.Windows.Forms.Button qbitBtn;
+        private System.Windows.Forms.Button notepadBtn;
+        private System.Windows.Forms.Button firefoxBtn;
+        private System.Windows.Forms.Button keepassBtn;
+        private System.Windows.Forms.Button libreBtn;
+        private System.Windows.Forms.Button githubBtn;
+        private System.Windows.Forms.Button irfanBtn;
+        private System.Windows.Forms.Button cpuzBtn;
+        private System.Windows.Forms.Button iTunesBtn;
+        private System.Windows.Forms.Button zipBtn;
+        private System.Windows.Forms.Button visualstudioBtn;
+        private System.Windows.Forms.Button bcuninstallerBtn;
+        private System.Windows.Forms.Button steamBtn;
+        private System.Windows.Forms.Button telegramBtn;
+    }
+}
+
---
+++ CleanWindowsProject/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CleanWindowsProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            EnforceAdminPrivilegesWorkaround();
+        }
+
+        private void edgeRemoveBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Remove-Edge.bat");
+        }
+
+        private void discordBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Discord.bat");
+        }
+
+        private void executeScript(string script)
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            //p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = script;
+            //p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            //outputBox.Text = "";
+            //while (!p.HasExited) { outputBox.Text += p.StandardOutput.ReadLine(); }
+            p.WaitForExit();
+        }
+
+        public static void EnforceAdminPrivilegesWorkaround()
+        {
+            RegistryKey rk;
+            string registryPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\";
+
+            try
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    rk = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                }
+                else
+                {
+                    rk = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                }
+
+                rk = rk.OpenSubKey(registryPath, true);
+            }
+            catch (System.Security.SecurityException)
+            {
+                MessageBox.Show("Please run as administrator");
+                Environment.Exit(1);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void zipBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("7-Zip.bat");
+        }
+
+        private void iTunesBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("iTunes.bat");
+        }
+
+        private void cpuzBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("CPU-Z.bat");
+        }
+
+        private void githubBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Github.bat");
+        }
+
+        private void irfanBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("IrfanView.bat");
+        }
+
+        private void wemodBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Wemod.bat");
+        }
+
+        private void qbitBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("qBittorrent.bat");
+        }
+
+        private void keepassBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("KeePass.bat");
+        }
+
+        private void libreBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("LibreOffice.bat");
+        }
+
+        private void visualstudioBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("VisualStudio.bat");
+        }
+
+        private void steamBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Steam.bat");
+        }
+
+        private void firefoxBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("FireFox.bat");
+        }
+
+        private void notepadBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Notepad++.bat");
+        }
+
+        private void gpuzBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("GPU-Z.bat");
+        }
+
+        private void veracryptBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("VeraCrypt.bat");
+        }
+
+        private void vlcBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("VLC.bat");
+        }
+
+        private void windirstatBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Windirstat.bat");
+        }
+
+        private void bcuninstallerBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("BCUninstaller.bat");
+        }
+
+        private void installAllBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("InstallAll.bat");
+        }
+
+        private void telegramBtn_Click(object sender, EventArgs e)
+        {
+            executeScript("Telegram.bat");
+        }
+    }
+}
